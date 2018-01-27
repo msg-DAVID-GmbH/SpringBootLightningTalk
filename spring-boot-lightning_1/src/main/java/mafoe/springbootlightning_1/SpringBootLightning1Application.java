992c34d4a5f3c725fbda1b703ee9606707a48cec
Martin Försterling <martinleanderfoersterling@gmail.com>
2018-01-27 11:40:26 +0100

    initial commit

---
+++ spring-boot-lightning_1/src/main/java/mafoe/springbootlightning_1/SpringBootLightning1Application.java
@@ -0,0 +1,12 @@
+package mafoe.springbootlightning_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLightning1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLightning1Application.class, args);
+	}
+}
---
+++ spring-boot-lightning_1/src/test/java/mafoe/springbootlightning_1/SpringBootLightning1ApplicationTests.java
@@ -0,0 +1,16 @@
+package mafoe.springbootlightning_1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootLightning1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
